c2f7f9f728ac051879e3e76c6251015ce5911ec3
Emile Bergeron <contact+github@prismalstudio.com>
2012-01-03 01:53:49 +0000

    Tile Engine

---
+++ src/TileEngine/World.cpp
@@ -0,0 +1,18 @@
+/*
+ * World.cpp
+ *
+ *  Created on: 2011-12-18
+ *      Author: Emile
+ */
+
+#include "World.h"
+
+World::World() {
+	// TODO Auto-generated constructor stub
+
+}
+
+World::~World() {
+	// TODO Auto-generated destructor stub
+}
+
