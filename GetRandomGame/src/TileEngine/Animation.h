414b5a6a54a964ed302976a021c80685195605dc
Emile Bergeron <contact+github@prismalstudio.com>
2012-01-12 05:13:07 +0000

    Separate Animation from Sprite and created a Layer enum

--- GetRandomGame/src/TileEngine/Animation.h
+++ GetRandomGame/src/TileEngine/Animation.h
@@ -1,92 +1,73 @@
 /*
- * Sprite.h
+ * Animation.h
  *
  *  Created on: 2012-01-08
  *      Author: Emile
  */
 
-#ifndef SPRITE_H_
-#define SPRITE_H_
+#ifndef ANIMATION_H_
+#define ANIMATION_H_
 
 #include <string>
+#include "Sprite.h"
 #include "SDL/SDL.h"
 //#include "SDLInterface.h"
 
-class Animation {
+class Animation: public Sprite {
+
 public:
 	Animation();
-	Animation(int, int, std::string, int = 255);
-	Animation(int, int, int, int, std::string, int, int = 255);
-	Animation(int, int, SDL_Surface*, int = 255);
+	Animation(int x, int y, std::string filename, int alpha = 255) :
+			Sprite(x, y, filename, alpha) {
+		_nbImage = 1; // only 1 image in the sprite sheet
+		_clip = NULL;
+	}
+
+	Animation(int x, int y, SDL_Surface * surface, int alpha = 255) :
+			Sprite(x, y, surface, alpha) {
+		_nbImage = 1;
+		_clip = NULL;
+	}
 	Animation(int, int, Animation *);
-	virtual ~Animation();
-	int getX() const {
-		return _x;
-	}
-	int getY() const {
-		return _y;
+	Animation(int, int, int, int, std::string, int, int, int = 255);
+
+	/**
+	 * GET functions
+	 */
+
+	int getFrametTime(){
+		return _frameTime;
 	}
 
-	int getAlpha() const {
-		return _alpha;
-	}
-
-	int getWidth() const {
-		return _width;
-	}
-	int getHeight() const {
-		return _height;
-	}
-
-	int getNbImage() {
+	virtual int getNbImage() {
 		return _nbImage;
 	}
 
-	std::string getFileName() {
-		return _fileName;
-	}
-
-	SDL_Surface * getSurface() {
-		return _spriteSheet;
-	}
-
-	SDL_Rect * getClipRect() {
+	virtual SDL_Rect * getClipRect() {
 		return _clip;
 	}
 
-	SDL_Rect * getClip(){
+	virtual SDL_Rect * getClip() {
 		return &_clip[_frame % _nbImage];
 	}
 
-	void setPoint(int x, int y) {
-		_x = x;
-		_y = y;
-	}
-	void setSize(int w, int h) {
-		_width = w;
-		_height = h;
-	}
 
-	void setAlpha(int alpha = 255) {
-		if ((alpha > 255) || (alpha < 0)) {
-			alpha = 255;
-		}
-		_alpha = alpha;
-	}
+	/**
+	 * SET functions
+	 */
+	void setSprite(std::string); // Do nothing, for futur uses
+	virtual void nextFrame(int time);
 
-	void setSprite(std::string);
-	void nextFrame(){
-		_frame++;
+	virtual void reset() {
+		_frame = 0;
 	}
 
 private:
-	int _x, _y, _width, _height, _alpha, _nbImage, _frame;
-	SDL_Surface * _spriteSheet;
+	int _nbImage, _frame, _lastUpdate, _frameTime;
 	SDL_Rect * _clip;
-	std::string _fileName;
 
 	void placeRect(int, int, int);
 	void setRect(int, int, int, int = 0, int = 0);
 };
 
-#endif /* SPRITE_H_ */
+#endif /* ANIMATION_H_ */
--- GetRandomGame/src/TileEngine/Sprite.h
+++ GetRandomGame/src/TileEngine/Sprite.h
@@ -7,11 +7,75 @@
 
 #ifndef SPRITE_H_
 #define SPRITE_H_
+#include <string>
+#include "SDL/SDL.h"
+//#include "SDLInterface.h"
 
 class Sprite {
 public:
 	Sprite();
+	Sprite(int, int, std::string, int = 255);
+	Sprite(int, int, SDL_Surface*, int = 255);
+	Sprite(int, int, Sprite *);
 	virtual ~Sprite();
+
+	int getX() const {
+		return _x;
+	}
+	int getY() const {
+		return _y;
+	}
+
+	int getAlpha() const {
+		return _alpha;
+	}
+
+	int getWidth() const {
+		return _width;
+	}
+	int getHeight() const {
+		return _height;
+	}
+	std::string getFileName() {
+		return _fileName;
+	}
+
+	SDL_Surface * getSurface() {
+		return _spriteSheet;
+	}
+
+	int getNbImage() {
+		return 1;
+	}
+	void setPoint(int x, int y) {
+		_x = x;
+		_y = y;
+	}
+	void setSize(int w, int h) {
+		_width = w;
+		_height = h;
+	}
+
+	void setAlpha(int alpha = 255) {
+		if ((alpha > 255) || (alpha < 0)) {
+			alpha = 255;
+		}
+		_alpha = alpha;
+	}
+
+	virtual SDL_Rect * getClip() {
+		return NULL;
+	}
+	virtual SDL_Rect * getClipRect() {
+		return NULL;
+	}
+	virtual void nextFrame(int);
+	virtual void reset();
+
+protected:
+	int _x, _y, _width, _height, _alpha;
+	SDL_Surface * _spriteSheet;
+	std::string _fileName;
 };
 
 #endif /* SPRITE_H_ */
