4d4994cc01dbbd0240967704d55171dcf9a8fc0b
Emile Bergeron <contact+github@prismalstudio.com>
2012-01-12 02:53:24 +0000

    Re-added Sprite, the new generic class

---
+++ GetRandomGame/src/TileEngine/Sprite.cpp
@@ -0,0 +1,18 @@
+/*
+ * Sprite.cpp
+ *
+ *  Created on: 2012-01-11
+ *      Author: Emile
+ */
+
+#include "Sprite.h"
+
+Sprite::Sprite() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Sprite::~Sprite() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ GetRandomGame/src/TileEngine/Sprite.h
@@ -0,0 +1,17 @@
+/*
+ * Sprite.h
+ *
+ *  Created on: 2012-01-11
+ *      Author: Emile
+ */
+
+#ifndef SPRITE_H_
+#define SPRITE_H_
+
+class Sprite {
+public:
+	Sprite();
+	virtual ~Sprite();
+};
+
+#endif /* SPRITE_H_ */
