b24df533be63e0d118ab92048287bc244829334b
ber.emile@gmail.com <ber.emile@gmail.com@da3f8d9e-3796-321e-ad5f-bdf298374293>
2012-02-24 01:40:31 +0000

    format, added return int in place (a regarder par DUPA)

--- trunk/GetRandomGame/src/GetRandomGame/ZoneType/docks.cpp
+++ trunk/GetRandomGame/src/GetRandomGame/ZoneType/docks.cpp
@@ -2,61 +2,56 @@
 #include "ZoneType/dockQuarter.h"
 #include <stdlib.h>
 
-docks::docks()
-{
-    probabilite=4;
-    size=1;
+docks::docks() {
+	probabilite = 4;
+	size = 1;
 }
-docks::~docks()
-{
+docks::~docks() {
 
 }
 
-docks::docks(char** map, point* p) : zoneType()
-{
-    map[p->Getx()][p->Gety()]='d';
+docks::docks(char** map, point* p) :
+		zoneType() {
+	map[p->Getx()][p->Gety()] = 'd';
 }
-int docks::place(char** map, std::vector<point*>* possible, int mapsize)
-{
-    bool found=false;
-    int i=rand()%((int)(possible->size()));
-    while(!found)
-    {
-        //on va cherche a traver le vecteur de points possible le premier point qui aura un voisin qui n'est pas de l'eau
-        i%=possible->size();
-        int j=-1;
-        while(++j<3&&!found)
-        {
-            int newX = possible->at(i)->Getx()+j-1;
-            if(newX>=0&&newX<mapsize)
-            {
+int docks::place(char** map, std::vector<point*>* possible, int mapsize) {
+	bool found = false;
+	int i = rand() % ((int) (possible->size()));
+	while (!found) {
+		//on va cherche a traver le vecteur de points possible le premier point qui aura un voisin qui n'est pas de l'eau
+		i %= possible->size();
+		int j = -1;
+		while (++j < 3 && !found) {
+			int newX = possible->at(i)->Getx() + j - 1;
+			if (newX >= 0 && newX < mapsize) {
 
-                if(map[newX][possible->at(i)->Gety()]!='e'&&map[newX][possible->at(i)->Gety()]!='k'&&map[newX][possible->at(i)->Gety()]!='Q')
-                {
-                     found=true;
-                     map[newX][possible->at(i)->Gety()]='Q';
+				if (map[newX][possible->at(i)->Gety()] != 'e'
+						&& map[newX][possible->at(i)->Gety()] != 'k'
+						&& map[newX][possible->at(i)->Gety()] != 'Q') {
+					found = true;
+					map[newX][possible->at(i)->Gety()] = 'Q';
 
-                     return(i);
-                }
-            }
+					return (i);
+				}
+			}
 
-        }
-        j=-1;
-        while(++j<3&&!found)
-        {
-            int newY = possible->at(i)->Gety()+j-1;
-           if(newY>=0&&newY<mapsize)
-            {
-                if(map[possible->at(i)->Getx()][newY]!='e'&&map[possible->at(i)->Getx()][newY]!='Q'&&map[possible->at(i)->Getx()][newY]!='k')
-                {
-                     found=true;
-                     map[possible->at(i)->Getx()][newY]='Q';
+		}
+		j = -1;
+		while (++j < 3 && !found) {
+			int newY = possible->at(i)->Gety() + j - 1;
+			if (newY >= 0 && newY < mapsize) {
+				if (map[possible->at(i)->Getx()][newY] != 'e'
+						&& map[possible->at(i)->Getx()][newY] != 'Q'
+						&& map[possible->at(i)->Getx()][newY] != 'k') {
+					found = true;
+					map[possible->at(i)->Getx()][newY] = 'Q';
 
-                     return(i);
-                }
-            }
+					return (i);
+				}
+			}
 
-        }
-        i++;
-    }
+		}
+		i++;
+	}
+	return -1; // TODO check that DUPA
 }
